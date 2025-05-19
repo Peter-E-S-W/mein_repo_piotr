1da526ce737e6f6de8138225d39057c95037e6ef
Piotr E.S. Wilinski <wilinski@uni-potsdam.de>
2025-05-19 15:27:51 +0200

    Initial commit

---
+++ mein_bash_skript.sh
@@ -0,0 +1,12 @@
+# Das Hashtag "#" macht die Zeile zu einem KOMMENTAR, wodurch diese Zeile vom Parser IGNORIERT wird.(z.B., wenn ich nach 10 Jahren zum Skript zurückkomme und Kommentare brauche, um es zu verstehen.)
+echo "Hallo zusammen."
+
+for FILE in *txt
+do
+echo "$FILE"
+done
+
+
+
+echo "Skript beendet."
+
